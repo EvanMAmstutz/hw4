9f0e17756403792af367a630d959ecb9060e06f0
Evan Amstutz <evanamstutz@Evans-MacBook-Air.local>
2015-06-17 10:06:24 -0400

    Initial Commit

---
+++ hw4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw4
+//
+//  Created by Evan Amstutz on 6/17/15.
+//  Copyright (c) 2015 Evan Amstutz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
